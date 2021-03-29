4982691d1b963187361d3ad38c148b19cbfe36a2
Samia Ferdous <samia.ferdous@icloud.com>
2021-03-28 23:00:26 -0400

    JS Functions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,58 @@
 //Moment.js - Time Display//
 $("#currentDay").text(moment().format("LLLL"));
 
+//saveBTN Click Listener//
+$(".saveBtn").on("click", function () {
+
+//HTML ID attributes//
+console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+//Local Storage//
+localStorage.setItem(time, text);
+    })
+
+//Set up Time Blocks for storage//
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour18 .description").val(localStorage.getItem("hour18"));
+$("#hour19 .description").val(localStorage.getItem("hour19"));
+$("#hour20 .description").val(localStorage.getItem("hour20"));  
+
+
+function hourTracker() {
+    var currentHour = moment().hour();
+
+    //Create a Timeblock Loop//
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+        //In order to see if the time has gone past//
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker();
